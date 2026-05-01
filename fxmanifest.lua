2301a2e24d5fe4dfca805f7766b58343b8991e2a
theo2577 <mmaximalefurry@gmail.com>
2026-05-01 14:47:44 +0200

    Create fxmanifest.lua for NovaCore-RP
    Initialize fxmanifest for NovaCore framework.

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'NovaCore Team'
+description 'Base principale du framework NovaCore-RP'
+version '1.0.0'
+
+-- C'est ici qu'on déclarera tes futurs scripts
+client_scripts {
+    -- 'client/main.lua'
+}
+
+server_scripts {
+    -- 'server/main.lua'
+}
